95a700df1146a3e47f5d2e5c4a071a0b957bbad9
ANUSREEPURUSHOTHAMANstartupmission <121005558+ANUSREEPURUSHOTHAMANstartupmission@users.noreply.github.com>
2023-10-07 12:55:46 +0530

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ANUSREEPURUSHOTHAMANstartupmission.github.io',
-  base:'/iedc_internship',
+  base:'/iedc_website',
   integrations: [tailwind()]
 });
